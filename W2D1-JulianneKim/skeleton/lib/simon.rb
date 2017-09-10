b80771566c06199c7401cc46993cc76370f9846f
Julee117 <juliannne@gmail.com>
2017-09-10 01:02:56 -0400

    Add methods

--- W2D1-JulianneKim/skeleton/lib/simon.rb
+++ W2D1-JulianneKim/skeleton/lib/simon.rb
@@ -10,35 +10,58 @@ class Simon
   end
 
   def play
+    until @game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    !@game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+    end
   end
 
   def require_sequence
-
+    puts "Please repeat the sequence"
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color != user_color[0]
+        @game_over = true
+        break 
+      end
+    end
   end
 
   def add_random_color
     colors = %w(red blue yellow green)
-    @seq << colors.sample 
+    @seq << colors.sample
   end
 
   def round_success_message
-
+    puts "You passed the round!"
   end
 
   def game_over_message
-
+    puts "Game over!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
