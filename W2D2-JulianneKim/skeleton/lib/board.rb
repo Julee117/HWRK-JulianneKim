4cd975263164b854f89ffdb03b6954934568dbac
Julee117 <juliannne@gmail.com>
2017-09-20 22:55:21 -0400

    Movie buff and N+1 Buster

--- W2D2-JulianneKim/skeleton/lib/board.rb
+++ W2D2-JulianneKim/skeleton/lib/board.rb
@@ -21,13 +21,39 @@ class Board
 
   def valid_move?(start_pos)
     raise "Invalid starting cup" if start_pos < 0 || start_pos > 7
+    raise "Invalid starting cup" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_index = start_pos
+    until stones.empty?
+      cup_index += 1
+      cup_index = 0 if cup_index > 13
+      if cup_index == 6
+        @cups[6] << stones.shift if current_player_name == @name1
+      elsif cup_index == 13
+        @cups[13] << stones.shift if current_player_name == @name2
+      else
+        @cups[cup_index] << stones.shift
+      end
+    end
+
+    render
+    next_turn(cup_index)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -44,5 +70,12 @@ class Board
   end
 
   def winner
+    if @cups[6].count > @cups[13].count
+      @name1
+    elsif @cups[6].count < @cups[13].count
+      @name2
+    else
+      :draw
+    end
   end
 end
